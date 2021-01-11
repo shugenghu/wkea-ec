2792e6c8bedd7e7a37fe9416e2f2aebd85217fcd
xiaoma-bigman <hao.ma@wkea.cn>
2021-01-11 16:17:04 +0800

    添加售后中心

--- build/public/static/js/bundle.js
+++ build/public/static/js/bundle.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6fee6f05bfcc29f65ed5";
+/******/ 	var hotCurrentHash = "384a36159b4d39279f6e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
