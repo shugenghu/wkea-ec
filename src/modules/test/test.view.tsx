f9a5c7ff35abbe9dd968f924f3e8587fa3fbe01d
xiaoma-bigman <hao.ma@wkea.cn>
2021-01-14 10:40:01 +0800

    tijiao

--- src/modules/test/test.view.tsx
+++ src/modules/test/test.view.tsx
@@ -9,8 +9,7 @@ import { ITestViewProps } from './test';
 export default (props: ITestViewProps) => {
     return (
         <div className='row'>
-            <h2>Config Value: {props.config.showText}</h2>
-            <h2>Resource Value: {props.resources.resourceKey}</h2>
+            test
         </div>
     );
 };
