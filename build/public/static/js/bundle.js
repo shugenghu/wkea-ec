3d4d17e9d33367c380688663b38580af3158eff9
xiaoma-bigman <hao.ma@wkea.cn>
2021-01-25 10:01:14 +0800

    调整距离

--- build/public/static/js/bundle.js
+++ build/public/static/js/bundle.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "24a6ce948c064a721529";
+/******/ 	var hotCurrentHash = "afdfac48e4df9fc6f38c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
